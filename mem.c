12eddcdcbd567ca75c2e6139d4b3d70fedf1a3bb
haknyan <hak.nyan@gmail.com>
2020-07-09 23:54:14 +0900

    commit

--- mem.c
+++ mem.c
@@ -1,5 +1,17 @@
 #include <stdio.h>
 
 int main() {
-    
+    char c, c_array[100];
+    int i, i_array[100];
+    short s, s_array[100];
+    float f, f_array[100];
+    long l, l_array[100];
+
+    printf("char size = %ld \t char array size = %4ld\n", sizeof(c), sizeof(c_array));
+    printf("int size = %ld \t int array size = %4ld\n", sizeof(i), sizeof(i_array));
+    printf("short size = %ld \t short array size = %4ld\n", sizeof(s), sizeof(s_array));
+    printf("float size = %ld \t flaot array size = %4ld\n", sizeof(f), sizeof(f_array));
+    printf("long size = %ld \t long array size = %4ld\n", sizeof(l), sizeof(l_array));
+
+    return 0;
 }
