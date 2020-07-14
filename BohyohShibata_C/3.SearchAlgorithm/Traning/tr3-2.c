b9689d74c9650897a71a584b68cc5c851baa3aba
haknyan <hak.nyan@gmail.com>
2020-07-14 21:40:11 +0900

    commit

--- BohyohShibata_C/3.SearchAlgorithm/Traning/tr3-2.c
+++ BohyohShibata_C/3.SearchAlgorithm/Traning/tr3-2.c
@@ -2,16 +2,13 @@
 #include <stdlib.h>
 
 int search(const int a[], int n, int key) {
-    int i = 0;
+    int i;
 
-    while (1) {
-        if (i == n){
-            return -1;
-        }
-        if (a[i] == key) {
+    for (i = 0; i < n; i++) {
+        if(a[i] == key) {
             return i;
-        }  
-        i++;
+        }
+        return -1;
     }
 }
 
