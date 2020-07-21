aa00790ebc6efc6d9229e6302b8a7dd3e9ab8bb1
haknyan <hak.nyan@gmail.com>
2020-07-21 21:47:51 +0900

    commit

--- BohyohShibata_C/4.Stack_Queue/Traning/IntStack.c
+++ BohyohShibata_C/4.Stack_Queue/Traning/IntStack.c
@@ -55,25 +55,21 @@ int IsFull(const IntStack *s) {
 
 int Search(const IntStack *s, int x) {
     int i;
-    for(i = s->ptr - 1; i >= 0; i--) {
-        if(s->stk[i] == x) {
+    for(i = s->ptr - 1; i >= 0; i--)
+        if(s->stk[i] == x)
             return i;
-        }
-    }
     return -1;
 }
 
 void Print(const IntStack *s) {
     int i;
-    for(i = 0; i < s->ptr; i++) {
+    for(i = 0; i < s->ptr; i++)
         printf("%d", s->stk[i]);
-    }
     putchar( '\n');
 }
 
 void Terminate(IntStack *s) {
-    if(s->stk != NULL) {
+    if(s->stk != NULL)
         free(s->stk);
-    }
     s->max = s->ptr = 0;
 }
