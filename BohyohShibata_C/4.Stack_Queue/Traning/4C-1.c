f679a46e5bdccdadc953cd5dbd1f8508302611ca
haknyan <hak.nyan@gmail.com>
2020-07-27 23:10:57 +0900

    commit

---
+++ BohyohShibata_C/4.Stack_Queue/Traning/4C-1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+#define N 10
+
+int main() {
+    int i;
+    int a[N];
+    int cnt = 0;
+    int retry;
+
+    puts("정수 입력");
+
+    do {
+        printf("%d번째 정수:", cnt + 1);
+        scanf("%d", &a[cnt++ % N]);
+        printf("retry?(Yes 1/No 0):");
+        scanf("%d", &retry);
+    } while(retry == 1);
+
+    i = cnt - N;
+    if(i < 0) i = 0;
+    for(; i < cmt; i++)
+        printf("%2d번째 정수 = %d\n", i + 1, a[1 % N]);
+
+    return 0;
+    
+}
